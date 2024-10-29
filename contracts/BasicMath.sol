5096286c8830058779b1e8cc266d7a7c540ecabc
keremgurel <keremgurell99@gmail.com>
2024-10-29 01:34:52 -0700

    bypass default overflow checks

--- contracts/BasicMath.sol
+++ contracts/BasicMath.sol
@@ -1,22 +1,22 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
 
 contract BasicMath {
     function adder(uint _a, uint _b) external pure returns (uint sum, bool error) {
-        sum = _a + _b;
-        if (sum < _a) {
+        unchecked {
+            sum = _a + _b;
+        }
+        if (sum < _a) { 
             return (0, true);
         }
         return (sum, false);
     }
 
     function subtractor(uint _a, uint _b) external pure returns (uint difference, bool error) {
-        difference = _a - _b;
-        if(difference < 0) {
+        if (_a < _b) { 
             return (0, true);
         }
-
+        difference = _a - _b;
         return (difference, false);
     }
-
 }
