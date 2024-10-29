08f07592344f03a515eda39fc2d86c174de3b16f
keremgurel <keremgurell99@gmail.com>
2024-10-29 01:10:39 -0700

    adder and subtractor

--- contracts/Greeting.sol
+++ contracts/Greeting.sol
@@ -1,14 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.7;
 
-contract Greeting {
-    function sayHello() external pure returns (string memory) {
-        return "hello world";
+contract BasicMath {
+    function adder(uint _a, uint _b) external pure returns (uint sum, bool error) {
+        sum = _a + _b;
+        if (sum < _a) {
+            return (0, true);
+        }
+        return (sum, false);
     }
 
-    function Greeter(string memory _name) external pure returns (string memory, string memory) {
-        return ("Hello", _name);
+    function subtractor(uint _a, uint _b) external pure returns (uint difference, bool error) {
+        difference = _a - _b;
+        if(difference < 0) {
+            return (0, true);
+        }
+
+        return (difference, false);
     }
 
-    // pure functions: doesn't access the state of the contract
 }
